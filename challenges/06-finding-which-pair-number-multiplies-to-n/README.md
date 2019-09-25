3e7b93760ec26b201bd7949283c605128443d115
Roger Russel <roger.russel@dafiti.com.br>
2019-09-25 14:48:40 -0300

    solutiong with map

---
+++ challenges/06-finding-which-pair-number-multiplies-to-n/README.md
@@ -0,0 +1,5 @@
+# 06 - Finding which pair number multiplies to n
+
+Find inside an array which number can be multiplied to n.
+
+f(n, [k...]), k1 * k2 = n
---
+++ solutions/06-finding-which-pair-numver-multiplies-to-n/golang/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func generateArray() []int {
+	return []int{1, 3, 4, 64, 323, 12, 34, 11, 4, 5, 6, 7, 8, 9, 123, 2, 20}
+}
+
+func main() {
+	n, _ := strconv.Atoi(os.Args[1])
+	arr := generateArray()
+
+	result := FindWithMap(n, arr)
+	fmt.Println(result)
+}
+
+// FindWithMap is O(n)
+func FindWithMap(n int, arr []int) string {
+	desirebles := make(map[int]int)
+
+	if n < 0 {
+		return "n>0"
+	}
+
+	if n == 0 {
+		return fmt.Sprintf("n * 0 = 0")
+	}
+
+	for _, value := range arr {
+
+		pair, remainder := n/value, n%value
+
+		if remainder > 0 {
+			continue
+		}
+
+		if v, ok := desirebles[value]; ok {
+			return fmt.Sprintf("%v * %v = %v", value, v, n)
+		}
+
+		desirebles[pair] = value
+
+	}
+
+	return ""
+}
